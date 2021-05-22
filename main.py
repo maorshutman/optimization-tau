1bac1e37b5969ff9e2944765cf78f37cff4816aa
maorshutman <maorus12@gmail.com>
2021-05-22 18:34:01 +0300

    Initial implementation of BFGS

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ class NewtonOptimizer:
         num_iters = 0
         while num_iters < self.max_iters:
 
-            # One step if always perfromed since the stopping creterion is
+            # One step if always perfromed since the stopping criterion is
             # |x_k+1 - x_k|.
             self.step()
 
@@ -91,7 +91,7 @@ class NewtonOptimizer:
             if norm_dx < self.threshold:
                 break
 
-            # print(f"{num_iters}: ", self.curr_x, self.objective(self.curr_x))
+            # print(f"{num_iters}: ", self.curr_x, norm_dx, self.objective(self.curr_x))
 
         fmin = self.objective(self.curr_x)
 
@@ -111,28 +111,131 @@ class BFGSOptimizer:
         threshold: float,
         max_iters: int
     ) -> None:
-        pass
+        """
+        Args:
+            x_0       : vector, initial guess for the minimizer
+            B_0       : matrix , initial guess of the inverse Hessian
+            alpha_0   : scalar, initial step size for Armijo line search
+            beta      : scalar, beta parameter of Armijo line search, a float
+                        in range (0,1)
+            sigma     : scalar, sigma parameter of Armijo line search, a float
+                        in range (0,1)
+            threshold : scalar, stopping criteria |𝑥_k+1 − x_k| < threshold,
+                        return x_k+1
+            max_iters : scalar, maximal number of iterations (stopping criteria)
+
+        """
+
+        self.objective = objective
+        self.x_0 = x_0
+        self.B_0 = B_0
+        self.alpha_0 = alpha_0
+        self.beta = beta
+        self.sigma = sigma
+        self.threshold = threshold
+        self.max_iters = max_iters
+
+        self.curr_x = self.x_0
+        self.B = B_0
+        self.next_x = None
+        self.next_d = None
+        self.alpha = None
 
     def update_dir(self) -> np.ndarray:
-        pass
+        """ Computes step direction. """
+        gx = self.objective.grad(self.curr_x)
+        self.next_d = -np.matmul(self.B, gx)
+        return self.next_d
 
     def update_step_size(self) -> np.ndarray:
-        pass
+        """ Armijo rule. """
+
+        h = lambda a : self.objective(self.curr_x + a * self.next_d) - self.objective(self.curr_x)
+        assert h(0) == 0.
+
+        # g = h'(0)
+        # TODO: What if g >= 0 ? set g = 0 ?
+        g = self.objective.grad(self.curr_x).dot(self.next_d)
+
+        alpha = self.alpha_0
+
+        if h(alpha) > self.sigma * g * alpha:
+            # Decrease alpha.
+            while True:
+                alpha = alpha * self.beta
+                if h(alpha) <= self.sigma * g * alpha:
+                    self.alpha = alpha
+                    break
+        else:
+            # Increase alpha.
+            while True:
+                prev_alpha = alpha
+                alpha = alpha / self.beta
+                if h(alpha) > self.sigma * g * alpha:
+                    self.alpha = prev_alpha
+                    break
+
+        return self.alpha
 
     def update_x(self) -> np.ndarray:
-        pass
+        self.next_x = self.curr_x + self.alpha * self.next_d
 
     def update_inv_hessian(
         self,
         prev_x: np.ndarray
     ) -> np.ndarray:
-        pass
+        """ Rank 2 update. """
+
+        p_k = self.alpha * self.next_d
+        q_k = self.objective.grad(self.next_x) - self.objective.grad(prev_x)
+
+        S_k = np.matmul(self.B, q_k)
+        tau_k = np.matmul(S_k.T, q_k)
+        mu_k = p_k.dot(q_k)
+
+        if (mu_k < 1e-10) or (tau_k < 1e-10):
+            return self.B
+
+        v_k = p_k / mu_k - S_k / tau_k
+
+        # print(mu_k, tau_k)
+        self.B = self.B + np.outer(p_k, p_k) / mu_k - np.outer(S_k, S_k) / tau_k + tau_k * np.outer(v_k, v_k)
+
+        return self.B
 
     def step(self) -> Tuple:
-        pass
+        self.update_dir()
+        self.update_step_size()
+        self.update_x()
+        self.update_inv_hessian(prev_x=self.curr_x)
+
+        return (self.next_x, self.next_d, self.alpha, self.B)
 
     def optimize(self) -> Tuple:
-        pass
+        num_iters = 0
+        while num_iters < self.max_iters:
+
+            # One step if always performed since the stopping criterion is
+            # |x_k+1 - x_k|.
+            self.step()
+
+            dx = self.next_x - self.curr_x
+            norm_dx = np.sqrt(dx.dot(dx))
+
+            # After `norm_dx` is computed, `curr_x is` not needed anymore.
+            self.curr_x = self.next_x
+            self.next_x = None
+
+            num_iters += 1
+
+            if norm_dx < self.threshold:
+                break
+
+            # print(f"{num_iters}: ", self.curr_x, norm_dx, self.objective(self.curr_x))
+
+        fmin = self.objective(self.curr_x)
+
+        return (fmin, self.curr_x, num_iters)
 
 
 class TotalVariationObjective:
--- tests.py
+++ tests.py
@@ -2,12 +2,17 @@ import numpy as np
 
 from main import NewtonOptimizer
 from main import QuadraticFunction
+from main import BFGSOptimizer
 
 
 def check_solution(x1, x2, epsilon=1e-6):
     d = x1 - x2
     norm_d = np.sqrt(d.dot(d))
     assert norm_d < epsilon
+
+
+def is_pos_def(x):
+    return np.all(np.linalg.eigvals(x) > 0)
 
 
 def newton_opt_test_0():
@@ -91,11 +96,138 @@ def newton_opt_test_3():
     print(newton_opt_test_3.__name__, (fmin, minimizer, num_iters))
 
 
+def newton_opt_test_4():
+    A = np.array([[1., 2., 1],
+                  [2., 1., 3],
+                  [1., 3, 7.]])
+    Q = np.matmul(A, A.T)
+
+    assert is_pos_def(Q)
+
+    b = np.array([2, 3, -1.])
+    x_0 = np.array([10., -9, 3.])
+
+    qf = QuadraticFunction(Q, b)
+
+    newton_opt = NewtonOptimizer(
+        qf, x_0=x_0, threshold=1e-10, alpha=0.5, max_iters=1000)
+
+    fmin, minimizer, num_iters = newton_opt.optimize()
+
+    print(newton_opt_test_4.__name__, (fmin, minimizer, num_iters))
+
+
+def bfgs_opt_test_0():
+    Q = np.array([[1., 2.],
+                  [2., 1.]])
+
+    b = np.array([2, 3])
+    x_0 = np.array([0., 0.])
+
+    qf = QuadraticFunction(Q, b)
+
+    B_0 = np.linalg.pinv(qf.hessian(x_0))
+
+    bfgs_opt = BFGSOptimizer(
+        objective=qf,
+        x_0=x_0,
+        B_0=B_0,
+        alpha_0=1.,
+        beta=0.2,
+        sigma=0.3,
+        threshold=1e-10,
+        max_iters=1000
+    )
+
+    fmin, minimizer, num_iters = bfgs_opt.optimize()
+
+    x_opt_expected = np.array([-1.33333333, -0.33333333])
+    check_solution(x_opt_expected, minimizer)
+
+    print(bfgs_opt_test_0.__name__, (fmin, minimizer, num_iters))
+
+
+def bfgs_opt_test_1():
+
+    A = np.array([[1., 2., 1],
+                  [2., 1., 3],
+                  [1., 3, 7.]])
+    Q = np.matmul(A, A.T)
+
+    assert is_pos_def(Q)
+
+    b = np.array([2, 3, -1.])
+    x_0 = np.array([10., -9, 3.])
+
+    qf = QuadraticFunction(Q, b)
+
+    B_0 = np.linalg.pinv(qf.hessian(x_0))
+
+    bfgs_opt = BFGSOptimizer(
+        objective=qf,
+        x_0=x_0,
+        B_0=B_0,
+        alpha_0=1.,
+        beta=0.2,
+        sigma=0.3,
+        threshold=1e-10,
+        max_iters=1000
+    )
+
+    fmin, minimizer, num_iters = bfgs_opt.optimize()
+
+    x_opt_expected = np.array([-0.46260388, -1.20221607,0.6565097])
+    check_solution(x_opt_expected, minimizer)
+
+    print(bfgs_opt_test_1.__name__, (fmin, minimizer, num_iters))
+
+
+def bfgs_opt_test_2():
+    """ Initialize Hessian with identity. """
+
+    A = np.array([[1., 2., 1],
+                  [2., 1., 3],
+                  [1., 3, 7.]])
+    Q = np.matmul(A, A.T)
+
+    assert is_pos_def(Q)
+
+    b = np.array([2, 3, -1.])
+    x_0 = np.array([10., -9, 3.])
+
+    qf = QuadraticFunction(Q, b)
+
+    B_0 = np.eye(3)
+
+    bfgs_opt = BFGSOptimizer(
+        objective=qf,
+        x_0=x_0,
+        B_0=B_0,
+        alpha_0=1.,
+        beta=0.2,
+        sigma=0.3,
+        threshold=1e-10,
+        max_iters=1000
+    )
+
+    fmin, minimizer, num_iters = bfgs_opt.optimize()
+
+    x_opt_expected = np.array([-0.46260388, -1.20221607,0.6565097])
+    check_solution(x_opt_expected, minimizer)
+
+    print(bfgs_opt_test_2.__name__, (fmin, minimizer, num_iters))
+
+
 def main():
     newton_opt_test_0()
     newton_opt_test_1()
     newton_opt_test_2()
     newton_opt_test_3()
+    newton_opt_test_4()
+
+    bfgs_opt_test_0()
+    bfgs_opt_test_1()
+    bfgs_opt_test_2()
 
     print()
 
